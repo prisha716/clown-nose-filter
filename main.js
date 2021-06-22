61b462ca9debaad76d54a1b49cf6c6cd9c6cd8aa
prisha716 <84237696+prisha716@users.noreply.github.com>
2021-06-22 19:58:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 noseX="";
 noseY="";
 function preload() {
-
+    clown_nose=loadImage('https://i.postimg.cc/wxmndbkx/download.png');
 }
  function setup() {
      canvas=createCanvas(300,300);
@@ -22,17 +22,15 @@ function preload() {
 
     
     console.log(results);
-    noseX=results[0].pose.nose.x;
-    noseY=results[0].pose.nose.y;
+    noseX=results[0].pose.nose.x-10;
+    noseY=results[0].pose.nose.y-10;
      console.log("nose x=" + results[0].pose.nose.x);
      console.log("nose y=" + results[0].pose.nose.y);}
 
  }
  function draw() {
 image(video,0,0,300,300);
-fill(255,0,0);
-stroke(255,0,0);
-circle(noseX.noseY,20);
+image(clown_nose,noseX,noseY,30,30);
  }
 function take_snapshot() {
     save('myfilter.png');
